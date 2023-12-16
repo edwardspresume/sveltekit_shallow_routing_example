8ffe9beb6064187205424a9bf9c7081083f1713c
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 16:40:01 -0500

    refactor(images/server.ts): update image URLs to use higher resolution images from the Picsum API, and get images starting from 10 instead of 1

--- src/routes/api/images/+server.ts
+++ src/routes/api/images/+server.ts
@@ -3,11 +3,13 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
 
-      const images = [...Array(20)].map((_, i) => ({
-        id: i,
-        alt: 'random image',
-        src: `https://picsum.photos/id/${i}/500`,
-      }));
-
+    const images = [...Array(20)].map((_, i) => {
+        i += 10;
+        return {
+          id: i,
+          alt: 'random image',
+          src: `https://picsum.photos/id/${i}/800`,
+        };
+    });
     return json(images);
 };
