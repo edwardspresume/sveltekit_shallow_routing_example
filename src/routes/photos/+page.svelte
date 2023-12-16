382b8300822e508d83cc4188148f8cf26437937f
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 17:45:45 -0500

    Add photo click functionality and photo dialog

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,17 +1,63 @@
 <script lang="ts">
+	import { goto, preloadData, pushState } from '$app/navigation';
 	import type { PageData } from './$types';
+
+	import { page } from '$app/stores';
+	import { route } from '$lib/ROUTES';
+
+	import * as Dialog from '$lib/components/ui/dialog';
+	import ImagePage from './[photoId]/+page.svelte';
 
 	export let data: PageData;
 
 	const { images } = data;
+
+	async function onPhotoClick(event: MouseEvent & { currentTarget: HTMLAnchorElement }) {
+		if (event.metaKey || event.ctrlKey) return;
+
+		event.preventDefault();
+
+		const { href } = event.currentTarget;
+
+		const result = await preloadData(href);
+
+		if (result['type'] === 'loaded' && result['status'] === 200) {
+			pushState(href, { photo: result['data'] });
+		} else {
+			goto(href);
+		}
+	}
+
+	let isPhotoDialogOpen = false;
+
+	$: if ($page.state.photo) {
+		isPhotoDialogOpen = true;
+	} else {
+		isPhotoDialogOpen = false;
+	}
 </script>
 
 {#if !images}
 	<p>No images found.</p>
 {:else}
-	<div class="mx-auto space-y-8 w-fit">
+	<div class="grid gap-8 mx-auto w-fit">
 		{#each images as image}
-			<img class="rounded shadow-md" src={image.src} alt={image.alt} />
+			<a href={route('/photos/[photoId]', { photoId: image.id })} on:click={onPhotoClick}>
+				<img class="rounded shadow-md" src={image.src} alt={image.alt} />
+			</a>
 		{/each}
 	</div>
 {/if}
+
+<Dialog.Root
+	open={isPhotoDialogOpen}
+	onOpenChange={(open) => {
+		if (!open) {
+			history.back();
+		}
+	}}
+>
+	<Dialog.Content>
+		<ImagePage data={$page.state.photo} />
+	</Dialog.Content>
+</Dialog.Root>
