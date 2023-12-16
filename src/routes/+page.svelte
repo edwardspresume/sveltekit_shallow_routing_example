d9b9a303531b27e9a1bf90d8e024a2d3b1dfb894
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 16:09:09 -0500

    Delete page.server.ts and link to users page in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+	import { route } from '$lib/ROUTES';
+</script>
+
 <h1 class="text-2xl font-bold text-center">SvelteKit Shallow Routing Examples</h1>
+
+<nav class="mx-auto mt-10 w-fit">
+	<a class="text-xl text-blue-600" href={route('/users')}>View users</a>
+</nav>
