796598eabbe290a56c9b756c74a97df85a01ab35
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 12:35:07 -0500

    Refactor user data loading in page server

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -1,15 +1,20 @@
 import type { PageServerLoad } from './$types';
 
 type User = { id: number; username: string; email: string };
+type UserData = { users: User[] };
 
-type UserData = {
-    users: User[];
-  };
-
-export const load = (async ({fetch}) => {
+export const load: PageServerLoad = async ({ fetch }) => {
+  try {
     const response = await fetch('https://dummyjson.com/users?limit=15&select=id,username,email');
-	const data = await response.json() as UserData;
-    const users = data.users
 
-    return {users};
-}) satisfies PageServerLoad;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = (await response.json()) as UserData;
+    return { users: data.users };
+  } catch (error) {
+    console.error('Failed to load users:', error);
+    return {users: []};
+  }
+};
