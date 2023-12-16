a212e74bbbf579ea3d77a08f7ead6a7a36659d45
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 14:09:22 -0500

    Remove layout server file

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-import { superValidate } from 'sveltekit-superforms/server';
-
-import { FeedbackValidationSchema } from '$validations/feedbackValidationSchema';
-
-export const load = (async () => {
-	return { feedbackForm: await superValidate(FeedbackValidationSchema) };
-}) satisfies LayoutServerLoad;
