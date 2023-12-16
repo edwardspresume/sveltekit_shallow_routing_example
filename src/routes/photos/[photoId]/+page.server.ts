c043bf411d87a0a9c8d1ee0491363cf035b5cee1
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 17:45:33 -0500

    Add image loading logic to +page.server.ts

--- src/routes/photos/[photoId]/+page.server.ts
+++ src/routes/photos/[photoId]/+page.server.ts
@@ -1,5 +1,15 @@
+import type { ImageType } from '$lib/types';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
+export const load = (async ({fetch, params: {photoId}}) => {
+    async function getImages () {
+
+        const response = await fetch('/api/images');
+        const images = await response.json() as ImageType[];
+        return images.find(image => image.id === +photoId)  as ImageType;
+    }
+
+    return {
+        image: await getImages()
+    };
 }) satisfies PageServerLoad;
