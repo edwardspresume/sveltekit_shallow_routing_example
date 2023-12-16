c902365d9c673239392a02ab2a8f871a72ffd446
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 16:32:19 -0500

    Add image fetching to page load

--- src/routes/photos/+page.server.ts
+++ src/routes/photos/+page.server.ts
@@ -1,7 +1,11 @@
+import type { ImageType } from '$lib/types';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({fetch}) => {
+    async function getImages () {
+        const response = await fetch('/api/images');
+        return await response.json() as ImageType
+    }
 
-    
-    return {};
+    return { images: await getImages()};
 }) satisfies PageServerLoad;
