926bc63929287f8a015c59756afc9f27e82458aa
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 16:47:03 -0500

    Add conditional rendering for images in photos page

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -2,4 +2,16 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const { images } = data;
 </script>
+
+{#if !images}
+	<p>No images found.</p>
+{:else}
+	<div class="mx-auto space-y-8 w-fit">
+		{#each images as image}
+			<img class="rounded shadow-md" src={image.src} alt={image.alt} />
+		{/each}
+	</div>
+{/if}
