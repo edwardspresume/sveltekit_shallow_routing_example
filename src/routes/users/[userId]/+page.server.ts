d82432943258bd2f8eaa295ff32532a445122811
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 12:38:32 -0500

    Refactor user page server load function

--- src/routes/users/[userId]/+page.server.ts
+++ src/routes/users/[userId]/+page.server.ts
@@ -1,5 +1,28 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    image: string;
+    username: string;
+    email: string;
+    phone: string;
+};
+
+export const load: PageServerLoad = async ({ fetch, params: { userId } }) => {
+    try {
+        const response = await fetch(`https://dummyjson.com/users/${userId}?select=firstName,lastName,image,username,email,phone`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const user = (await response.json()) as User;
+
+        return { user };
+    } catch (error) {
+        console.error('Failed to load user:', error);
+        return {user: null};
+    }
+};
