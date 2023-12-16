3ce7ce5cdd36f3527846f7587b98dd5fb7db3833
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 12:11:04 -0500

    Add user data fetching in load function

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -1,5 +1,15 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
+type User = { id: number; username: string; email: string };
+
+type UserData = {
+    users: User[];
+  };
+
+export const load = (async ({fetch}) => {
+    const response = await fetch('https://dummyjson.com/users?limit=15&select=id,username,email');
+	const data = await response.json() as UserData;
+    const users = data.users
+
+    return {users};
 }) satisfies PageServerLoad;
