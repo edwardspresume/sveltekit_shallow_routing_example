2cdc9c4f51fedeac76a1d6bccebae1ea48a7d146
Edwards Presume <edwardspresume@gmail.com>
2023-12-16 16:31:58 -0500

    Add buttons styling for viewing users and photos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Button from '$components/ui/button/button.svelte';
 	import { route } from '$lib/ROUTES';
 </script>
 
 <h1 class="text-2xl font-bold text-center">SvelteKit Shallow Routing Examples</h1>
 
-<nav class="mx-auto mt-10 w-fit">
-	<a class="text-xl text-blue-600" href={route('/users')}>View users</a>
+<nav class="mx-auto mt-10 space-x-3 w-fit">
+	<Button href={route('/users')}>View users</Button>
+	<Button href={route('/photos')}>View photos</Button>
 </nav>
